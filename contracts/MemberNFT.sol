02003e101f61e32aebfd58a040b5d9f2c53a6236
Yuki Matsumoto <drying@athomegeek.local>
2023-01-28 20:43:48 +0900

    ライブラリimportとconstructor追加

--- contracts/MemberNFT.sol
+++ contracts/MemberNFT.sol
@@ -2,6 +2,12 @@
 
 pragma solidity ^0.8.9;
 
-contract MemberNFT {
-        
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MemberNFT is ERC721Enumerable, ERC721URIStorage, Ownable{
+    constructor() ERC721("MemberNFT", "MEM") {}
 }
