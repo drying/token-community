acf567360c2b910fca80949c49ebe1e2b339bf1b
Yuki Matsumoto <drying@athomegeek.local>
2023-01-30 10:20:12 +0900

    MemberNFT:OZインポートと継承,オーバーライド

--- contracts/MemberNFT.sol
+++ contracts/MemberNFT.sol
@@ -10,4 +10,40 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MemberNFT is ERC721Enumerable, ERC721URIStorage, Ownable{
     constructor() ERC721("MemberNFT", "MEM") {}
+
+    /**
+     * @dev
+     * - オーバーライド
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev
+     * - オーバーライド
+     */
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    /**
+     * @dev
+     * - オーバーライド
+     */
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev
+     * - オーバーライド
+     */
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
 }
