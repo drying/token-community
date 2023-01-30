f297078ba3f63f069a08b12230b926ae196d37c8
Yuki Matsumoto <drying@athomegeek.local>
2023-01-30 10:36:31 +0900

    テストレイアウト作成

---
+++ test/MemberNFT.js
@@ -0,0 +1,10 @@
+const {expect} = require("Chai");
+
+describe("MemberNFTコントラクト", function() {
+    it("トークンの名前とシンボルがセットされるべき", async function(){
+        expect().to.equal(0);
+    });
+    it("デプロイアドレスがOwnerに設定されるべき", async function(){
+        expect().to.equal(0);
+    });
+})
