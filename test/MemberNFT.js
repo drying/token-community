fa188ae78ff92c627cffa6338892b068b7fe7e44
Yuki Matsumoto <drying@athomegeek.local>
2023-02-01 09:21:27 +0900

    MemberNFT:constructorの実装

--- test/MemberNFT.js
+++ test/MemberNFT.js
@@ -1,10 +1,21 @@
 const {expect} = require("Chai");
+const { ethers } = require("hardhat");
 
 describe("MemberNFTコントラクト", function() {
     it("トークンの名前とシンボルがセットされるべき", async function(){
-        expect().to.equal(0);
+        const name = "MemberNFT";
+        const symbol = "MEM";
+        const MemberNFT = await ethers.getContractFactory("MemberNFT");
+        const memberNFT = await MemberNFT.deploy();
+        await memberNFT.deployed();
+        expect(await memberNFT.name()).to.equal(name);
+        expect(await memberNFT.symbol()).to.equal(symbol);
     });
     it("デプロイアドレスがOwnerに設定されるべき", async function(){
-        expect().to.equal(0);
+        const [owner] = await ethers.getSigners();
+        const MemberNFT = await ethers.getContractFactory("MemberNFT");
+        const memberNFT = await MemberNFT.deploy();
+        await memberNFT.deployed();
+        expect(await memberNFT.owner()).to.equal(owner.address);
     });
 })
