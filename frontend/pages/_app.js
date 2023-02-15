c57a3680de5a6b181c6ce0407d05647085d0caae
Yuki Matsumoto <drying@athomegeek.local>
2023-02-15 09:28:34 +0900

    fronend:app.jsのエラー処理

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
+/* import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+}*/
+
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
